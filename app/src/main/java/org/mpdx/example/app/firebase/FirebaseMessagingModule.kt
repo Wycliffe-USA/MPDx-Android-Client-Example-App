39ebf09652cbb8efb51c1b8ad3f6e751b7723537
Gyasi Story <gyasistory@gmail.com>
2022-01-18 13:56:37 -0500

    Create FirebaseMessagingModule.kt

---
+++ app/src/main/java/org/mpdx/example/app/firebase/FirebaseMessagingModule.kt
@@ -0,0 +1,9 @@
+package org.mpdx.example.app.firebase
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class FirebaseMessagingModule
