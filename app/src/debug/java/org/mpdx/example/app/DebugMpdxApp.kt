7fbff4ec65fa1e26b9a41c4376af7a0d153254b7
Gyasi Story <gyasistory@gmail.com>
2022-01-18 13:55:16 -0500

    Create DebugMpdxApp.kt

---
+++ app/src/debug/java/org/mpdx/example/app/DebugMpdxApp.kt
@@ -0,0 +1,35 @@
+package org.mpdx.example.app
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import leakcanary.LeakCanary
+import okhttp3.logging.HttpLoggingInterceptor
+import org.ccci.gto.android.common.leakcanary.crashlytics.CrashlyticsOnHeapAnalyzedListener
+import org.ccci.gto.android.common.okhttp3.util.addGlobalNetworkInterceptor
+import timber.log.Timber
+
+class DebugMpdxApp : MpdxApp() {
+    override fun onCreate() {
+        configLeakCanary()
+
+        enableOkHttpLogging()
+        Timber.plant(Timber.DebugTree())
+
+        super.onCreate()
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    private fun configLeakCanary() {
+        LeakCanary.config = LeakCanary.config.copy(
+            onHeapAnalyzedListener = CrashlyticsOnHeapAnalyzedListener
+        )
+    }
+
+    private fun enableOkHttpLogging() {
+        addGlobalNetworkInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+    }
+}
