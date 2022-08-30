d41a09c448cb31ddd2e5b5426fcd229aec8de1a7
Gyasi Story <gyasi.story@cru.org>
2022-08-30 11:52:05 -0400

    Add Okta Listener logic
    Update SplashActivity.kt

--- app/src/main/java/org/mpdx/example/features/splash/SplashActivity.kt
+++ app/src/main/java/org/mpdx/example/features/splash/SplashActivity.kt
@@ -3,7 +3,9 @@ package org.mpdx.example.features.splash
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import com.okta.oidc.AuthorizationStatus
 import com.okta.oidc.clients.web.WebAuthClient
+import com.okta.oidc.util.AuthorizationException
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import org.ccci.gto.android.common.okta.oidc.clients.sessions.isAuthenticatedLiveData
@@ -11,6 +13,8 @@ import org.mpdx.android.core.modal.ModalActivity
 import org.mpdx.android.features.base.BaseActivity
 import org.mpdx.android.features.onboarding.OnboardingActivity
 import org.mpdx.android.features.secure.UnlockFragment
+import org.mpdx.android.library.okta.listener.OktaActivityListener
+import timber.log.Timber
 
 fun Activity.startSplashActivity() = startActivity(Intent(this, SplashActivity::class.java))
 
@@ -18,7 +22,7 @@ fun Activity.startSplashActivity() = startActivity(Intent(this, SplashActivity::
  * This class serves as the entry point into the MPDx Application.
  */
 @AndroidEntryPoint
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(), OktaActivityListener {
     @Inject
     internal lateinit var oktaClient: WebAuthClient
 
@@ -78,4 +82,30 @@ class SplashActivity : BaseActivity() {
 
     override fun showUnlockScreen() = false
     override val pageName = "Splash"
+
+    /**
+     * Method called when login is canceled with a result.
+     */
+    override fun onCancel() {
+        // Reload when Login Canceled
+        startSplashActivity()
+        finish()
+    }
+    /**
+     * Method called on error with a the authorized request call.
+     *
+     * @param msg       error message
+     * @param exception The [com.okta.oidc.util.AuthorizationException] type of exception
+     */
+    override fun onError(msg: String?, exception: AuthorizationException?) {
+        // Track errors logged by Okta
+        Timber.e(exception, msg)
+    }
+
+    /**
+     * Method called on authorized with a result.
+     *
+     * @param result Result of the authorized request.
+     */
+    override fun onSuccess(result: AuthorizationStatus) {}
 }
